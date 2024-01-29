49b47ea770f4998734997c8751dda41b19d131a5
adrianpalacio <145379361+adrianpalacio@users.noreply.github.com>
2024-01-29 14:39:42 +0000

    Update Command.java

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Command {
+package software.ulpgc.kata5;
+
+public interface Command {
+    Output execute(Input input);
+
+    interface Input {
+        String get(String key);
+    }
+
+    interface Output {
+        String result();
+        int response();
+    }
+
 }
