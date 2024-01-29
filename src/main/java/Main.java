a919e3180af44b5ec77d1b1df13894d6f4a177dd
adrianpalacio <145379361+adrianpalacio@users.noreply.github.com>
2024-01-29 14:41:16 +0000

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Main {
+package software.ulpgc.kata5;
+
+import spark.Spark;
+
+public class Main {
+    public static void main(String[] args) {
+        CommandExecutor.put("factorial", new FactorialCommand());
+        Spark.port(8080);
+        Spark.get("/factorial", (req,res)-> CommandExecutor.with(req, res).execute("factorial"));
+    }
+
 }
