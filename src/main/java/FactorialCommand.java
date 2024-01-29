3cb895087ca8a4390e9c530a08700837c683dc7c
adrianpalacio <145379361+adrianpalacio@users.noreply.github.com>
2024-01-29 14:40:52 +0000

    Update FactorialCommand.java

--- src/main/java/FactorialCommand.java
+++ src/main/java/FactorialCommand.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class FactorialCommand {
+package software.ulpgc.kata5;
+
+import java.util.stream.IntStream;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.String.valueOf;
+
+public class FactorialCommand implements Command{
+    @Override
+    public Output execute(Input input) {
+        try {
+            return outputOf(factorial(input.get("number")));
+        }
+        catch (Exception e) {
+            return outputException();
+        }
+    }
+
+    private Output outputException() {
+        return new Output() {
+            @Override
+            public String result() {
+                return null;
+            }
+
+            @Override
+            public int response() {
+                return 403;
+            }
+        };
+    }
+
+    private Output outputOf(String result) {
+        return new Output() {
+            @Override
+            public String result() {
+                return result;
+            }
+
+            @Override
+            public int response() {
+                return 200;
+            }
+        };
+    }
+
+    private static String factorial(String number) {
+        return valueOf(factorial(parseInt(number)));
+    }
+
+    private static int factorial(int number) {
+        return IntStream.range(2,number+1).reduce(1,(a, i)->a*i);
+    }
+
 }
